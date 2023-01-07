fe4a38267ea97b42baf2daf821dafee4ce5368ab
Atanas <atanas.chebishev@gmail.com>
2023-01-08 00:30:01 +0200

    Syntax, Functions and Statements - Lab 7/10

---
+++ 01. Syntax, Functions and Statements/dayOfWeek.js
@@ -0,0 +1,34 @@
+function dayOfWeek(input) {
+    currentDay = input
+    output = ""
+    switch (currentDay) {
+        case "Monday":
+            output = 1
+            break
+        case "Tuesday":
+            output = 2
+            break
+        case "Wednesday":
+            output = 3
+            break
+        case "Thursday":
+            output = 4
+            break
+        case "Friday":
+            output = 5
+            break
+        case "Saturday":
+            output = 6
+            break
+        case "Sunday":
+            output = 7
+            break
+        default:
+            output = "error"
+            break
+    }
+    console.log(output)
+}
+dayOfWeek('Monday');
+dayOfWeek('Friday');
+dayOfWeek('Invalid');
